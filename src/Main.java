0ccf70dc5e326bdc2a727aebbb44bf0c75501b76
Rodrigo Avancini <rodrigo.avancini@thoughtful.ai>
2023-10-20 10:31:17 -0300

    feat: added TUI

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,33 @@
+import java.util.Objects;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Encrypt.encrypt("loddy", 9));
-        System.out.println(Encrypt.decrypt("kyoot", 9));
+        Scanner scanner = new Scanner(System.in);
+
+
+        Random rn = new Random();
+
+        System.out.println("Encrypt (1) or decrypt (2)?");
+        int option;
+        do {
+            option = scanner.nextInt();
+        } while (option != 1 && option != 2);
+
+        if (option == 1) {
+            System.out.print("Text to encrypt: ");
+            scanner.nextLine();
+            String text = scanner.nextLine();
+            int seed = Math.abs(rn.nextInt());
+            System.out.println("Encrypted text: " + Encrypt.encrypt(text, seed) + " \n\nseed: " + seed);
+        } else {
+            System.out.println("Text to decrypt: ");
+            scanner.nextLine();
+            String text = scanner.nextLine();
+            System.out.println("\nSeed: ");
+            int seed = scanner.nextInt();
+            System.out.println("Decrypted text: " + Encrypt.decrypt(text, seed));
+        }
     }
 }
